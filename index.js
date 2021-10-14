7153f1d2dab8c40878b0baebf0d8d0acf699133b
Duketamin <duketamin@Duketamins-MacBook-Pro.local>
2021-10-14 10:18:58 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
+const mongoose = require('mongoose');
+
+const typeDefs = require('./graphql/typeDefs')
+const resolvers = require('./graphql/resolvers');
+const { MONGODB } = require('./config.js')
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req }) => ({ req })
+});
+
+mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('MongoDB listen');
+        return server.listen({ port: 5000 });
+    })
+    .then(res => {
+        console.log(`Server running at ${res.url}`);
+    })
